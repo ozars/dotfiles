e97648a577b1701a5ef384c40827f6ac1b614ce8
Omer Ozarslan <omer@utdallas.edu>
2019-01-03 13:00:12 -0600

    fonts: Add setup for gnome terminal

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-DOTFILES="vimrc vim bashrc envrc zshrc oh-my-zsh tmux.conf"
+MYFONT="InconsolataGo NF 13"
+DOTFILES="vimrc vim bashrc envrc zshrc oh-my-zsh tmux.conf fonts"
 for conf in $DOTFILES
 do
-  ln $@ -s "dotfiles/$conf" "$HOME/.$conf"
+  ln -s -n $@ "dotfiles/$conf" "$HOME/.$conf"
 done
+
+# Font configuration
+fc-cache
+dpkg -s libglib2.0-bin >/dev/null 2>&1 || {
+  echo "Installing gsettings..."
+  sudo apt-get install -y libglib2.0-bin
+}
+gprofile_schema="org.gnome.Terminal.Legacy.Profile"
+gprofile_path=`gsettings get org.gnome.Terminal.ProfilesList default`
+gprofile_path="/org/gnome/terminal/legacy/profiles:/:${gprofile_path:1:-1}/"
+gsettings set "$gprofile_schema:$gprofile_path" "use-system-font" "false"
+gsettings set "$gprofile_schema:$gprofile_path" "font" "'$MYFONT'"
