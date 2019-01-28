8e52f1e354fe2bd3f1b3f5464e2fa55e438db897
Omer Ozarslan <omer@utdallas.edu>
2019-01-27 18:20:21 -0600

    setup: Update Dockerfile and install.sh
    - Configure Dockerfile for creating a copy of these dot configurations.
    - Expose ssh port of the docker image for tmux sharing.
    - Add option to skip gnome settings in install.sh since docker image
    won't have GUI installed.

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,13 @@ FROM debian:testing
 MAINTAINER omerfaruko AT gmail DOT com
 
 RUN DEBIAN_FRONTEND=noninteractive apt-get update
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y python3 python3-dev \
-build-essential autotools-dev git curl gettext locales
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y locales
 RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 ENV LANG en_US.UTF-8
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y vim-nox tmux zsh
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip
-RUN pip3 install --upgrade setuptools pip wheel
-RUN pip3 install "python-language-server[all]"
-WORKDIR /root
+COPY setup/buster-packages.sh dotfiles/setup/buster-packages.sh
+RUN DEBIAN_FRONTEND=noninteractive dotfiles/setup/buster-packages.sh
 COPY . dotfiles
-RUN ln -fs dotfiles/vim .vim && \
-    ln -fs dotfiles/vimrc .vimrc && \
-    ln -fs dotfiles/envrc .envrc && \
-    ln -fs dotfiles/bashrc .bashrc
-RUN vim -E -u NONE -S .vimrc +PlugInstall +qall || true
-ENTRYPOINT bash -i -c "vim --version; cd dotfiles && vim *; zsh"
+RUN DEBIAN_FRONTEND=noninteractive SKIP_GNOME=1 dotfiles/install.sh -nf
+EXPOSE 22
--- install.sh
+++ install.sh
@@ -4,42 +4,43 @@ MYFONT="InconsolataGo NF 13"
 DOTFILES="vimrc vim bashrc envrc zshrc oh-my-zsh oh-my-zsh-custom tmux.conf tmux fonts"
 for conf in $DOTFILES
 do
-  ln -s -n $@ "dotfiles/$conf" "$HOME/.$conf"
+  ln -s -n $@ "`realpath --relative-to=""$HOME"" ""$(dirname $0)""/$conf""`" "$HOME/.$conf"
 done
 
 # gnome-terminal configuration
+if [ -z $SKIP_GNOME ] || [ "$SKIP_GNOME" -eq 0 ]; then
+  dpkg -s libglib2.0-bin >/dev/null 2>&1 || {
+    echo "Installing gsettings..."
+    sudo apt-get install -y libglib2.0-bin
+  }
 
-dpkg -s libglib2.0-bin >/dev/null 2>&1 || {
-  echo "Installing gsettings..."
-  sudo apt-get install -y libglib2.0-bin
-}
+  gprofile_schema="org.gnome.Terminal.Legacy.Profile"
+  gprofile_path=`gsettings get org.gnome.Terminal.ProfilesList default`
+  gprofile_path="/org/gnome/terminal/legacy/profiles:/:${gprofile_path:1:-1}/"
+  function gprofile()
+  {
+    if [ "$#" -eq 1 ]
+    then
+      gsettings get "$gprofile_schema:$gprofile_path" "$1"
+      return $?
+    elif [ "$#" -eq 2 ]
+    then
+      gsettings set "$gprofile_schema:$gprofile_path" "$1" "$2"
+      return $?
+    else
+      2> echo "Unknown arguments for gsettings: $@"
+      exit 1
+    fi
+  }
 
-gprofile_schema="org.gnome.Terminal.Legacy.Profile"
-gprofile_path=`gsettings get org.gnome.Terminal.ProfilesList default`
-gprofile_path="/org/gnome/terminal/legacy/profiles:/:${gprofile_path:1:-1}/"
-function gprofile()
-{
-  if [ "$#" -eq 1 ]
-  then
-    gsettings get "$gprofile_schema:$gprofile_path" "$1"
-    return $?
-  elif [ "$#" -eq 2 ]
-  then
-    gsettings set "$gprofile_schema:$gprofile_path" "$1" "$2"
-    return $?
-  else
-    2> echo "Unknown arguments for gsettings: $@"
-    exit 1
-  fi
-}
+  # Font configuration
+  fc-cache "$HOME/.fonts"
+  gprofile "font" "'$MYFONT'"
+  gprofile "use-system-font" "false"
 
-# Font configuration
-fc-cache "$HOME/.fonts"
-gprofile "font" "'$MYFONT'"
-gprofile "use-system-font" "false"
-
-# Color palette configuration (solarized)
-gprofile "background-color" 'rgb(0,43,54)'
-gprofile "foreground-color" "'rgb(131,148,150)'"
-gprofile "palette" "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
-gprofile "use-theme-colors" "false"
+  # Color palette configuration (solarized)
+  gprofile "background-color" 'rgb(0,43,54)'
+  gprofile "foreground-color" "'rgb(131,148,150)'"
+  gprofile "palette" "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
+  gprofile "use-theme-colors" "false"
+fi
